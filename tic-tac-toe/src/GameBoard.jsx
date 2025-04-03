7906c3d1fd1ca1a6168284385a3247d6b7f69cb9
SanchitGta <sanchit.gupta@think41.com>
2025-04-03 10:23:58 +0530

    Auto-commit changes

--- tic-tac-toe/src/GameBoard.jsx
+++ tic-tac-toe/src/GameBoard.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
-function GameBoard() {
+function GameBoard({ currentPlayer, onCellClick }) {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handleClick = (index) => {
+    if (board[index] || !onCellClick) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    onCellClick(index);
+  };
+
   return (
     <div className="game-board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      {board.map((value, index) => (
+        <Square key={index} value={value} onClick={() => handleClick(index)} />
+      ))}
     </div>
   );
 }
