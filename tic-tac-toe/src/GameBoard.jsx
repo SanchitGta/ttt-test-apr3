b6ed729674c3530c2cc6c61a02df37a44e042172
SanchitGta <sanchit.gupta@think41.com>
2025-04-03 10:22:46 +0530

    Auto-commit changes

---
+++ tic-tac-toe/src/GameBoard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Square from './Square';
+
+function GameBoard() {
+  return (
+    <div className="game-board">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
+
+export default GameBoard;
